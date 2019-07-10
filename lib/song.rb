8f1d97c13f3db20c608df8d8db19b6cecdd74d6c
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-10 12:31:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,4 +2,22 @@ class Song
   def initialize(name)
     @name = name
   end
+  def @name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def @artist=(artist)
+    @artist = artist
+  end
+  def artist
+    @artist
+  end
+  def @genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
 end
